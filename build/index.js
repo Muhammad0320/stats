837c32e98c49bd64550ef3518516791049ed484f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-19 07:14:25 +0100

    chore: updaet index ts file

--- build/index.js
+++ build/index.js
@@ -1,3 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("shit");
+const fs_1 = __importDefault(require("fs"));
+fs_1.default.readFileSync("footbal.csv", { encoding: "utf-8" })
+    .split("\n")
+    .map((row) => {
+    return row.split(",");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-console.log("shit");
-
 import fs from "fs";
+
+fs.readFileSync("footbal.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
