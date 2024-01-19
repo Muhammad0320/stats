13a506fbd3b29cced7f8dcca01585b726824789f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-19 07:43:05 +0100

    chore: compare code

--- src/index.ts
+++ src/index.ts
@@ -19,4 +19,8 @@ for (const match of matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   }
+
+  if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
 }
