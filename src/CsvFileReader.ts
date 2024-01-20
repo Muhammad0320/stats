9376bdf749ff6c40d6bdb6aa46464e5423a45f3a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-20 06:57:21 +0100

    chore: update csv file reader

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,1 +1,15 @@
-export class CsvFileReader {}
+import fs from "fs";
+
+export class CsvFileReader {
+  data: string[][] = [];
+
+  constructor(public filename: string) {}
+
+  read(): void {
+    fs.readFileSync(this.filename, { encoding: "utf-8" })
+      .split("\n")
+      .map((row: string): string[] => {
+        return row.split(",");
+      });
+  }
+}
