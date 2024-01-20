bc2e87437790ef01322b6d57fa16a49aa471d719
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-20 09:08:42 +0100

    chore: created another Csv file for another refactor way

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -1,23 +1,27 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
-const fs_1 = __importDefault(require("fs"));
 class CsvFileReader {
     constructor(filename) {
         this.filename = filename;
         this.data = [];
     }
     read() {
-        this.data = fs_1.default
+        this.data = fs
             .readFileSync(this.filename, { encoding: "utf-8" })
             .split("\n")
             .map((row) => {
             return row.split(",");
         })
-            .map(this.mapRow);
+            .map((row) => [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5],
+            row[6],
+        ]);
     }
 }
 exports.CsvFileReader = CsvFileReader;
