d57bf6463e511fc04af5fad73d1f0c4e9752ee34
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-20 09:21:11 +0100

    chore: update MatchReader class

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,6 +1,31 @@
+import { MatchResult } from "./MatchResults";
+import { dateStringToDate } from "./utils";
+
 interface MatchType {
   data: string[][];
   read(): void;
 }
 
-export class MatchReader {}
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class MatchReader {
+  matches: MatchData[] = [];
+
+  constructor(public reader: MatchType) {}
+
+  load(): void {
+    this.reader.read();
+
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult,
+        row[6],
+      ]
+    );
+  }
+}
