7cb93971a72b5619360024dcb186bed936a91f26
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-20 09:09:35 +0100

    chore: update csvFileReader class

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -1,27 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
+const fs_1 = __importDefault(require("fs"));
 class CsvFileReader {
     constructor(filename) {
         this.filename = filename;
         this.data = [];
     }
     read() {
-        this.data = fs
+        this.data = fs_1.default
             .readFileSync(this.filename, { encoding: "utf-8" })
             .split("\n")
             .map((row) => {
             return row.split(",");
-        })
-            .map((row) => [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5],
-            row[6],
-        ]);
+        });
     }
 }
 exports.CsvFileReader = CsvFileReader;
