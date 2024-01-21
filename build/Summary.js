68fae5700c55233752840f4ffbd5b64a6bc44881
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-21 05:03:31 +0100

    chore:  update summary comp

--- build/Summary.js
+++ build/Summary.js
@@ -2,5 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Summary = void 0;
 class Summary {
+    constructor(analyser, outputTarget) {
+        this.analyser = analyser;
+        this.outputTarget = outputTarget;
+    }
 }
 exports.Summary = Summary;
--- src/Summary.ts
+++ src/Summary.ts
@@ -8,4 +8,6 @@ export interface OutputTarget {
   print(report: string): void;
 }
 
-export class Summary {}
+export class Summary {
+  constructor(public analyser: Analyser, public outputTarget: OutputTarget) {}
+}
