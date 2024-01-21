08166dca728e1ff8cfa6d9e83387b5b134f17dc8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-21 05:23:52 +0100

    chore: update wins Analysis

--- build/analyser/WinsAnalysis.js
+++ build/analyser/WinsAnalysis.js
@@ -1,1 +1,22 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WinsAnalyses = void 0;
+const MatchResults_1 = require("../MatchResults");
+class WinsAnalyses {
+    constructor(team) {
+        this.team = team;
+    }
+    run(matches) {
+        let wins = 0;
+        for (const match of matches) {
+            if (match[1] === "Man United" && match[5] === MatchResults_1.MatchResult.HomeWin) {
+                wins++;
+            }
+            if (match[2] === "Man United" && match[5] === MatchResults_1.MatchResult.AwayWin) {
+                wins++;
+            }
+        }
+        return ` ${this.team} won ${wins} games `;
+    }
+}
+exports.WinsAnalyses = WinsAnalyses;
