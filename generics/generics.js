648856a3e5940b9fe335603d87a1815ba8b26703
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-21 09:00:32 +0100

    chore: update gebetics

--- generics/generics.js
+++ generics/generics.js
@@ -9,3 +9,21 @@ class ArrayOfAnything {
 }
 new ArrayOfAnything(["a", "b", "6"]);
 new ArrayOfAnything([1, 2, 3, 4]);
+// Generic constraints
+class Cars {
+    print() {
+        console.log("I am a car");
+    }
+}
+class House {
+    print() {
+        console.log("I am a House");
+    }
+}
+const HouseAndCars = (collection) => {
+    for (let i = 0; i < collection.length; i++) {
+        console.log(collection[i].print());
+    }
+};
+HouseAndCars([new House(), new House()]);
+HouseAndCars([new Cars(), new Cars()]);
--- generics/generics.ts
+++ generics/generics.ts
@@ -9,3 +9,31 @@ class ArrayOfAnything<T> {
 new ArrayOfAnything<string>(["a", "b", "6"]);
 
 new ArrayOfAnything<number>([1, 2, 3, 4]);
+
+// Generic constraints
+
+class Cars {
+  print(): void {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print(): void {
+    console.log("I am a House");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+const HouseAndCars = <T extends Printable>(collection: T[]) => {
+  for (let i = 0; i < collection.length; i++) {
+    console.log(collection[i].print());
+  }
+};
+
+HouseAndCars<House>([new House(), new House()]);
+
+HouseAndCars<Cars>([new Cars(), new Cars()]);
