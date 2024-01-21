0a83fa495f600c94b85bdb908bcb48e5573effc7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-21 05:31:51 +0100

    chore: update index.ts

--- build/index.js
+++ build/index.js
@@ -1,19 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
-const MatchResults_1 = require("./MatchResults");
+const CsvFileReader_1 = require("./CsvFileReader");
 const fileRead = new CsvFileReader_1.CsvFileReader("football.csv");
 const reader = new MatchReader_1.MatchReader(fileRead);
 reader.load();
-let manUnitedWins = 0;
-for (const match of reader.matches) {
-    if (match[1] === "Man United" && match[5] === MatchResults_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    if (match[2] === "Man United" && match[5] === MatchResults_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(manUnitedWins);
-// muhammawwal@005
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResults";
+import { CsvFileReader } from "./CsvFileReader";
 
 const fileRead = new CsvFileReader("football.csv");
 
 const reader = new MatchReader(fileRead);
 
 reader.load();
-
-let manUnitedWins = 0;
-
-for (const match of reader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  }
-
-  if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(manUnitedWins);
-
-// muhammawwal@005
